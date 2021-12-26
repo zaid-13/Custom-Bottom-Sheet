9aa7dd12cd345ed3707ee8a1c1fcced642f056e4
Zaid Borkar <borkarzayd@gmail.com>
2021-12-26 23:56:24 +0530

    some changes

--- src/components/BottomSheet.js
+++ src/components/BottomSheet.js
@@ -6,11 +6,11 @@ const BottomSheet = () => {
 
     const translate = useRef(new Animated.Value(480)).current;
 
-    const pan = useRef(PanResponder.create({
+    const panResponder = useRef(PanResponder.create({
         onStartShouldSetPanResponder: () => true,
         onPanResponderGrant: () => {
-            translate.setOffset(translate._value);
             console.log("granted permission");
+            translate.setOffset(translate._value);  // take value from _value and put into offset
             console.log("showing translate._value in grant", translate._value);
         },
         onPanResponderMove: Animated.event([
@@ -23,22 +23,34 @@ const BottomSheet = () => {
             listener: (e, g) => console.log({ ...translate })
         }),
         onPanResponderRelease: (e, gestureState) => {
-            // translate.flattenOffset();
-            // if (translate._value < 0) {
-            //     Animated.spring(translate, {
-            //         toValue: 0,
-            //         duration: 2000,
-            //         useNativeDriver: true
-            //     }).start();
-            //     // translate.flattenOffset();
-            // } else if (translate._value > 0) {
-            //     Animated.spring(translate, {
-            //         toValue: 480,
-            //         duration: 2000,
-            //         useNativeDriver: true
-            //     }).start();
-            // }
-            translate.flattenOffset();
+            if (translate._value < -20) {
+                Animated.spring(translate, {
+                    toValue: 0,
+                    duration: 2000,
+                    useNativeDriver: true
+                }).start();
+                // translate._value = -480;   // 1. flattenOffset() will add this to _value 480  + (-480) = 0     2. then onGrant setOffset(translate._value) will take 0 from _value and put it in the offset
+
+                // here we are deliberately setting the value of ._value because
+                // when we animate the element to 0, _value and _offset have no
+                // idea about this movement, so next time when panresponder 
+                // grants permission setOffset() will set its own calculated value
+                // and not our own, giving unexpected results. 
+            }
+            console.log("inside if 1st condition", "value", translate._value, "offset", translate._offset);
+            if (translate._value > 50) {
+                Animated.spring(translate, {
+                    toValue: 480,
+                    duration: 2000,
+                    useNativeDriver: true
+                }).start();
+                translate._value = 480;
+                translate._offset = 480;
+                console.log("inside if 2nd condition", "value", translate._value, "offset", translate._offset);
+            }
+            translate.flattenOffset();   // add value to _value and reset offset
+            // translate._value = 0;
+            console.log({ ...translate })
 
         }
     })).current;
@@ -46,7 +58,7 @@ const BottomSheet = () => {
     return (
         <Animated.View
 
-            {...pan.panHandlers}
+            {...panResponder.panHandlers}
 
             style={{
                 backgroundColor: "pink",
