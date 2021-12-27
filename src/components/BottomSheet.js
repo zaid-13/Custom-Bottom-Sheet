8c8065e41fafe4520f1f860206d9fe6c80a0c785
shafiq <skshafiq483@gmail.com>
2021-12-27 17:13:44 +0530

    some changes

--- src/components/BottomSheet.js
+++ src/components/BottomSheet.js
@@ -6,29 +6,41 @@ const BottomSheet = () => {
 
     const translate = useRef(new Animated.Value(480)).current;
 
+
     const panResponder = useRef(PanResponder.create({
         onStartShouldSetPanResponder: () => true,
         onPanResponderGrant: () => {
-            console.log("granted permission");
+            console.log("on Grant -----------------------------------------------------------------");
             translate.setOffset(translate._value);  // take value from _value and put into offset
-            console.log("showing translate._value in grant", translate._value);
+            console.log("onGrant => offset", translate._offset);
+            console.log("onGrant => value", translate._value);
         },
-        onPanResponderMove: Animated.event([
-            null,
-            {
-                dy: translate
+        onPanResponderMove: (e, gestureState) => {
+            
+            if (gestureState.dy) {
+
+                console.log("onMove ------------------------------------------------------------------------")
+
+                console.log('onMove => offset', translate._offset);
+                console.log('onMove => value', translate._value);
+
+                return Animated.event([
+                    null,
+                    {
+                        dy: translate
+                    }
+                ], {
+                    useNativeDriver: false
+                })(e, gestureState)
             }
-        ], {
-            useNativeDriver: false,
-            listener: (e, g) => console.log({ ...translate })
-        }),
+        },
         onPanResponderRelease: (e, gestureState) => {
-            if (translate._value < -20) {
-                Animated.spring(translate, {
-                    toValue: 0,
-                    duration: 2000,
-                    useNativeDriver: true
-                }).start();
+            // if (translate._value < -20) {
+            //     Animated.spring(translate, {
+            //         toValue: 0,
+            //         duration: 2000,
+            //         useNativeDriver: true
+            //     }).start();
                 // translate._value = -480;   // 1. flattenOffset() will add this to _value 480  + (-480) = 0     2. then onGrant setOffset(translate._value) will take 0 from _value and put it in the offset
 
                 // here we are deliberately setting the value of ._value because
@@ -36,21 +48,53 @@ const BottomSheet = () => {
                 // idea about this movement, so next time when panresponder 
                 // grants permission setOffset() will set its own calculated value
                 // and not our own, giving unexpected results. 
-            }
-            console.log("inside if 1st condition", "value", translate._value, "offset", translate._offset);
-            if (translate._value > 50) {
+            // }
+            // console.log("inside if 1st condition", "value", translate._value, "offset", translate._offset);
+            // if (translate._value > 50) {
+            //     Animated.spring(translate, {
+            //         toValue: 480,
+            //         duration: 2000,
+            //         useNativeDriver: true
+            //     }).start();
+            //     translate._value = 480;
+            //     translate._offset = 480;
+            //     console.log("inside if 2nd condition", "value", translate._value, "offset", translate._offset);
+            // }
+            // translate.flattenOffset();   // add value to _value and reset offset
+            // // translate._value = 0;
+            // console.log({ ...translate })
+
+
+            if (translate._value < -30) {
+                Animated.spring(translate, {
+                    toValue: 0,
+                    duration: 2000,
+                    useNativeDriver: true
+                }).start()
+                translate._value = 0;
+                translate._offset = 0;
+
+            } else if (translate._value > 100) {
                 Animated.spring(translate, {
                     toValue: 480,
                     duration: 2000,
                     useNativeDriver: true
-                }).start();
-                translate._value = 480;
-                translate._offset = 480;
-                console.log("inside if 2nd condition", "value", translate._value, "offset", translate._offset);
+                }).start()
+                translate.setOffset(480);
+                translate.setValue(0);
+
+                // console.log("onRelease greater than 100");
             }
-            translate.flattenOffset();   // add value to _value and reset offset
-            // translate._value = 0;
-            console.log({ ...translate })
+
+            translate.flattenOffset();
+
+            console.log("onRelease ----------------------------------------------------------------------------")
+
+            console.log("onRelease => offset", translate._offset);
+            console.log("onRelease => value", translate._value);
+
+
+
 
         }
     })).current;
